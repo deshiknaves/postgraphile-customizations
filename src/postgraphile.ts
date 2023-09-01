2a1e2f14700eab1e790a6fd304fb9a9371edcf06
Deshi Rahim <nawal.rahim@gmail.com>
2023-08-31 22:22:02 -0700

    ✨ Add external api

--- src/postgraphile.ts
+++ src/postgraphile.ts
@@ -1,5 +1,36 @@
 import PgSimplifyInflectorPlugin from '@graphile-contrib/pg-simplify-inflector'
+import { gql, makeExtendSchemaPlugin } from 'graphile-utils'
 import { postgraphile as createPostgraphile } from 'postgraphile'
+
+const ExternalApiPlugin = makeExtendSchemaPlugin((build) => {
+  return {
+    typeDefs: gql`
+      type Photo {
+        albumId: Int
+        id: Int
+        title: String
+        url: String
+        thumbnailUrl: String
+      }
+
+      extend type Query {
+        photo: Photo
+      }
+    `,
+    resolvers: {
+      Query: {
+        photo: async () => {
+          const response = await fetch(
+            'https://jsonplaceholder.typicode.com/photos/1'
+          )
+          const json = await response.json()
+
+          return json
+        },
+      },
+    },
+  }
+})
 
 export const postgraphile = createPostgraphile(
   {
@@ -7,7 +38,7 @@ export const postgraphile = createPostgraphile(
   },
   'public',
   {
-    appendPlugins: [PgSimplifyInflectorPlugin],
+    appendPlugins: [PgSimplifyInflectorPlugin, ExternalApiPlugin],
     watchPg: true,
     graphiql: true,
     enhanceGraphiql: true,
